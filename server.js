f5f4315a496610cfa90319364d9d3ec6214eac80
Widlund, Emil <ewidlund@ea.com>
2018-10-08 18:18:12 +0200

    Implemented webpack-based internal project server

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-const express = require('express');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const path = require('path');
 
 const PORT = 8010;
 
 module.exports = (servePath, cb) => {
-    const app = express();
-    
-    app.use('/', express.static(servePath));
 
-    app.listen(PORT, () => {
-        console.log(`Server now running on port ${PORT}`);
-        if (cb) cb();
+    const config = {
+        entry: `${servePath}/Main.js`,
+        mode: 'development',
+        target: 'electron-renderer',
+        output: {
+            path: servePath,
+            filename: 'bundle.js'
+        }
+    }
+
+    const options = {
+        contentBase: servePath,
+        inline: true,
+        hot: true
+    };
+
+    const server = new WebpackDevServer(webpack(config), options);
+
+    server.listen(PORT, 'localhost', (err) => {
+        if (err) return console.error(err);
+
+        console.log('Serrver running on port', PORT);
+
+        cb();
     });
 }
